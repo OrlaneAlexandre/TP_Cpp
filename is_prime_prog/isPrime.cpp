19d3e01b3b96e4fc4a43a3613a6595513fa1ecac
OrlaneAlexandre <114063531+OrlaneAlexandre@users.noreply.github.com>
2022-10-05 02:09:17 -0400

    Add files via upload

---
+++ is_prime_prog/isPrime.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool isPrime(int nb)
+{
+	if (nb <= 1)
+	{
+		return false;
+	}
+	if (nb == 2)
+	{
+		return true;
+	}
+	for (int i = 2; i < ceil(sqrt(nb))+1; i++)
+	{
+		if (nb % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+void main()
+{
+	int nb;
+	cout << "Entrez des nombres : " << endl;
+	while (cin >> nb)
+	{
+		if (isPrime(nb))
+		{
+			cout << nb << ": le nombre est premier \n";
+		}
+		else
+		{
+			cout << nb << ": le nombre n'est pas premier \n";
+		}
+	}
+}
